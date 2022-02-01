de5b78a9d733e60ffabc50019593b802b95bea90
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-02-01 16:53:55 -0500

    update

--- backend/api/adapters/client.py
+++ backend/api/adapters/client.py
@@ -4,6 +4,7 @@ class Client:
     CLIENT_SECRET = "3JX3NRGRS2P0KE0NSKPTMCOZOY4MWUU4M3G33BO4XTRJ15SM"
     DATE = "20190407"
     URL = "https://api.foursquare.com/v2/venues/search"
+    SHOW_URL = "https://api.foursquare.com/v2/venues"
 
     def auth_params(self):
         return {'client_id': self.CLIENT_ID,
@@ -18,3 +19,26 @@ class Client:
     def request_venues(self):
         response = requests.get(self.URL, self.full_params())
         return response.json()['response']['venues']
+
+    def request_venue(self, venue_id):
+        response = requests.get(f"{self.SHOW_URL}/{venue_id}", self.auth_params())
+        return response.json()['response']['venue']
+
+# class RequestAndBuild:
+#     def __init__(self):
+#         self.client = adapters.Client()
+#         self.builder = adapters.Builder()
+#         self.conn = db.conn
+#         self.cursor = self.conn.cursor()
+
+
+#     def run(self, ll, query):
+#         search_params = {'ll': f"{ll}", "query": query}
+#         venues = self.client.request_venues(search_params)
+#         venue_foursquare_ids = [venue['id'] for venue in venues]
+#         venue_objs = []
+#         for foursquare_id in venue_foursquare_ids:
+#             venue_details = self.client.request_venue(foursquare_id)
+#             venue_obj = self.builder.run(venue_details, self.conn, self.cursor)
+#             venue_objs.append(venue_obj)
+#         return venue_objs
